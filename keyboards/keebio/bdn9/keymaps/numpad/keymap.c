4ffc62f68e246c1bf0a49170b6f193d35c54d99f
Kyle Penfound <kpenfound11@gmail.com>
2020-12-07 19:38:27 -0500

    add numpad layout

--- keyboards/keebio/bdn9/keymaps/numpad/keymap.c
+++ keyboards/keebio/bdn9/keymaps/numpad/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         | Left              | Down | Right              |
      */
     [0] = LAYOUT(
-        KC_MUTE, KC_P8, KC_P9,
+        KC_P7, KC_P8, KC_P9,
         KC_P4  , KC_P5  , KC_P6,
         KC_P1, KC_P2, KC_P3
     ),
@@ -40,9 +40,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 void encoder_update_user(uint8_t index, bool clockwise) {
-    if (clockwise) {
-        tap_code(KC_VOLU);
-    } else {
-        tap_code(KC_VOLD);
-    }
+
 }
